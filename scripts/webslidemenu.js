d24b1a3d9071bbaaa201abebc825229a04f37f16
Prem Kumar Rajbhar <premkumarrajbhar22@gmail.com>
2026-06-08 21:54:23 +0530

    added relevant scripts files

---
+++ scripts/webslidemenu.js
@@ -0,0 +1,190 @@
+function _0x1c93() {
+  var _0x5b7f02 = [
+    "body",
+    "li\x20>\x20.sub-menu",
+    "668750DKFKiG",
+    "wrapInner",
+    "626002yOZkiR",
+    ".overlapblackbg",
+    "siblings",
+    "css",
+    "children",
+    "click",
+    "<div\x20class=\x22wsmenucontainer\x22\x20/>",
+    "#wsnavtoggle",
+    "resize",
+    "prependTo",
+    ".wsmegamenu",
+    "min-width",
+    "touchstart",
+    "prepend",
+    ".wsmenu",
+    "<div\x20class=\x22overlapblackbg\x22></div>",
+    "<span\x20class=\x22wsmenu-click\x22><i\x20class=\x22wsmenu-arrow\x22></i></span>",
+    "width",
+    "height",
+    "slideToggle",
+    ".wsmenu-arrow",
+    "wsactive02",
+    "not",
+    "<span\x20class=\x22wsmenu-click02\x22><i\x20class=\x22wsmenu-arrow\x22></i></span>",
+    ".wsmenu\x20>\x20.wsmenu-list\x20>\x20li",
+    "wsactive",
+    "5360016QEUnJM",
+    "1377201FqpEnZ",
+    "41004RzIptc",
+    "1111188GGBzpc",
+    "wsmenu-rotate",
+    "16KfgHBh",
+    "slideUp",
+    "trigger",
+    "has",
+    "ws-activearrow",
+    "addEventListener",
+    "12mMDsTM",
+    "toggleClass",
+    ".wsmenu-click",
+    "parent",
+    "slow",
+    ".wsmenu02-list",
+    "2739276JhvJPQ",
+    ".sub-menu",
+    "removeClass",
+  ];
+  _0x1c93 = function () {
+    return _0x5b7f02;
+  };
+  return _0x1c93();
+}
+function _0x1d03(_0x48cf68, _0x554970) {
+  var _0x1c939e = _0x1c93();
+  return (
+    (_0x1d03 = function (_0x1d03dc, _0x48361b) {
+      _0x1d03dc = _0x1d03dc - 0x1d4;
+      var _0x9a38cf = _0x1c939e[_0x1d03dc];
+      return _0x9a38cf;
+    }),
+    _0x1d03(_0x48cf68, _0x554970)
+  );
+}
+((function (_0x4c44f5, _0x4cb368) {
+  var _0x228491 = _0x1d03,
+    _0x3824a8 = _0x4c44f5();
+  while (!![]) {
+    try {
+      var _0x16b8ae =
+        parseInt(_0x228491(0x1d6)) / 0x1 +
+        (-parseInt(_0x228491(0x1f2)) / 0x2) *
+          (-parseInt(_0x228491(0x1fb)) / 0x3) +
+        (-parseInt(_0x228491(0x1f5)) / 0x4) *
+          (parseInt(_0x228491(0x1d4)) / 0x5) +
+        parseInt(_0x228491(0x1f3)) / 0x6 +
+        -parseInt(_0x228491(0x1f1)) / 0x7 +
+        parseInt(_0x228491(0x1f0)) / 0x8 +
+        -parseInt(_0x228491(0x201)) / 0x9;
+      if (_0x16b8ae === _0x4cb368) break;
+      else _0x3824a8["push"](_0x3824a8["shift"]());
+    } catch (_0x172f85) {
+      _0x3824a8["push"](_0x3824a8["shift"]());
+    }
+  }
+})(_0x1c93, 0x80aff),
+  jQuery(
+    (function () {
+      "use strict";
+      var _0x273ed6 = _0x1d03;
+      (document[_0x273ed6(0x1fa)](_0x273ed6(0x1e2), function () {}, ![]),
+        jQuery(function () {
+          var _0x5b4d34 = _0x273ed6;
+          (jQuery("body")[_0x5b4d34(0x1d5)](_0x5b4d34(0x1dc)),
+            jQuery(_0x5b4d34(0x1e5))[_0x5b4d34(0x1df)](_0x5b4d34(0x1e4)),
+            jQuery(_0x5b4d34(0x1dd))[_0x5b4d34(0x1db)](function () {
+              var _0x4a1421 = _0x5b4d34;
+              jQuery(_0x4a1421(0x204))[_0x4a1421(0x1fc)](_0x4a1421(0x1ef));
+            }),
+            jQuery(_0x5b4d34(0x1d7))["click"](function () {
+              var _0x3e0354 = _0x5b4d34;
+              jQuery(_0x3e0354(0x204))[_0x3e0354(0x203)](_0x3e0354(0x1ef));
+            }),
+            jQuery(".wsmenu\x20>\x20.wsmenu-list\x20>\x20li")
+              [_0x5b4d34(0x1f8)](_0x5b4d34(0x202))
+              [_0x5b4d34(0x1e3)](_0x5b4d34(0x1e6)),
+            jQuery(_0x5b4d34(0x1ee))
+              ["has"](_0x5b4d34(0x1e0))
+              ["prepend"](_0x5b4d34(0x1e6)),
+            jQuery(_0x5b4d34(0x1fd))[_0x5b4d34(0x1db)](function () {
+              var _0x55b5ef = _0x5b4d34;
+              (jQuery(this)
+                [_0x55b5ef(0x1fc)](_0x55b5ef(0x1f9))
+                [_0x55b5ef(0x1fe)]()
+                [_0x55b5ef(0x1d8)]()
+                ["children"]()
+                ["removeClass"](_0x55b5ef(0x1f9)),
+                jQuery(
+                  ".wsmenu\x20>\x20.wsmenu-list\x20>\x20li\x20>\x20.sub-menu,\x20.wsmegamenu",
+                )
+                  [_0x55b5ef(0x1ec)](
+                    jQuery(this)[_0x55b5ef(0x1d8)](
+                      ".wsmenu\x20>\x20.wsmenu-list\x20>\x20li\x20>\x20.sub-menu,\x20.wsmegamenu",
+                    ),
+                  )
+                  [_0x55b5ef(0x1f6)](_0x55b5ef(0x1ff)),
+                jQuery(this)
+                  ["siblings"](".sub-menu")
+                  [_0x55b5ef(0x1e9)](_0x55b5ef(0x1ff)),
+                jQuery(this)
+                  [_0x55b5ef(0x1d8)](_0x55b5ef(0x1e0))
+                  ["slideToggle"]("slow"));
+            }),
+            jQuery(
+              ".wsmenu\x20>\x20.wsmenu-list\x20>\x20li\x20>\x20ul\x20>\x20li",
+            )
+              [_0x5b4d34(0x1f8)](_0x5b4d34(0x202))
+              [_0x5b4d34(0x1e3)](_0x5b4d34(0x1ed)),
+            jQuery(
+              ".wsmenu\x20>\x20.wsmenu-list\x20>\x20li\x20>\x20ul\x20>\x20li\x20>\x20ul\x20>\x20li",
+            )
+              [_0x5b4d34(0x1f8)](".sub-menu")
+              [_0x5b4d34(0x1e3)](
+                "<span\x20class=\x22wsmenu-click02\x22><i\x20class=\x22wsmenu-arrow\x22></i></span>",
+              ),
+            jQuery(".wsmenu-click02")[_0x5b4d34(0x1db)](function () {
+              var _0x199257 = _0x5b4d34;
+              (jQuery(this)
+                [_0x199257(0x1da)](_0x199257(0x1ea))
+                [_0x199257(0x1fc)](_0x199257(0x1f4)),
+                jQuery(this)
+                  ["siblings"](_0x199257(0x205))
+                  [_0x199257(0x1e9)](_0x199257(0x1ff)));
+            }),
+            jQuery(window)["on"]("resize", function () {
+              var _0x5d70d5 = _0x5b4d34;
+              (jQuery(_0x5d70d5(0x1e4))[_0x5d70d5(0x1d9)](
+                _0x5d70d5(0x1e8),
+                jQuery(this)[_0x5d70d5(0x1e8)]() + "px",
+              ),
+                jQuery(".wsmenucontainer")[_0x5d70d5(0x1d9)](
+                  _0x5d70d5(0x1e1),
+                  jQuery(this)[_0x5d70d5(0x1e7)]() + "px",
+                ));
+            }),
+            jQuery(window)[_0x5b4d34(0x1f7)](_0x5b4d34(0x1de)),
+            jQuery("#wsnavtoggle02")[_0x5b4d34(0x1db)](function () {
+              var _0xbeaad5 = _0x5b4d34;
+              jQuery(_0xbeaad5(0x204))["toggleClass"](_0xbeaad5(0x1eb));
+            }),
+            jQuery(".overlapblackbg")["click"](function () {
+              var _0x1f2d91 = _0x5b4d34;
+              jQuery("body")[_0x1f2d91(0x203)](_0x1f2d91(0x1eb));
+            }),
+            jQuery(window)["on"](_0x5b4d34(0x1de), function () {
+              var _0x554e15 = _0x5b4d34;
+              jQuery(_0x554e15(0x200))[_0x554e15(0x1d9)](
+                _0x554e15(0x1e8),
+                jQuery(this)["height"]() + "px",
+              );
+            }),
+            jQuery(window)[_0x5b4d34(0x1f7)]("resize"));
+        }));
+    })(),
+  ));
